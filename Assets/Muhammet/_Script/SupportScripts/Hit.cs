8ae554ad68febfb044f0d5ed05603d55b67eff78
Muhammet İNCE <83353864+MuhammetInceee@users.noreply.github.com>
2023-02-08 15:43:51 +0300

    Hit Effect Added

--- Assets/Muhammet/_Script/SupportScripts/Hit.cs
+++ Assets/Muhammet/_Script/SupportScripts/Hit.cs
@@ -7,11 +7,19 @@ namespace WreckingOI.SupportScripts.Collisions
 {
     public class Hit : MonoBehaviour
     {
+        private ParticleSystem _hitParticle;
+
+        private void Awake()
+        {
+            _hitParticle = transform.GetChild(0).GetComponent<ParticleSystem>();
+        }
+
         private void OnCollisionEnter(Collision collision)
         {
             if (collision.collider.TryGetComponent(out Character.Character character))
             {
                 CinemachineShake.Instance.ShakeCamera(1, 0.5f);
+                _hitParticle.Play();
             }
         }
     }
